ec488eac7b7cefbf848e141f393c118622860e07
Assami Muzaki <muzakiassami@gmail.com>
2023-02-15 22:43:01 +0700

    init next basic

---
+++ pages/blog.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <div className="">
+      <p>blog page</p>
+    </div>
+  );
+}
---
+++ pages/users/detail.tsx
@@ -0,0 +1,3 @@
+export default function UserDetail() {
+  return <p>User detail page</p>;
+}
